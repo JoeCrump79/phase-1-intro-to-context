4d2fec802c3ee790a964789ed6bc0dabb73a7824
Joseph Crump <josephcrump79@yahoo.com>
2024-10-09 09:34:24 -0500

    Implemented Function Signatures

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+function createEmployeeRecord(employeeData) {
+
+}
+
+function createEmployeeRecords(employeeData) {
+
+}
+
+function createTimeInEvent(employeeRecord, dateTimeStamp) {
+
+}
+
+function createTimeOutEvent(employeeRecord, dateTimeStamp) {
+
+}
+
+
+function hoursWorkedOnDate(employeeRecord, dateStamp) {
+
+}
+
+function wagesEarnedOnDate(employeeRecord, dateStamp) {
+
+}
+
+function allWagesFor(employeeRecord) {
+
+}
+
+function calculatePayroll(employeeRecords) {
+
+}
